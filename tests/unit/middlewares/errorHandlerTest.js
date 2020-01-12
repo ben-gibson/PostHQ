25d8ed69ddc948263069f4f2224ef44593a9ac01
Ben Gibson <ben.gibson@starlingbank.com>
2020-01-12 17:54:01 +0000

    Add error handler test

---
+++ tests/unit/middlewares/errorHandlerTest.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const middleware = require('@middlewares/errorHandler');
+const HttpError = require('@errors/HttpError');
+const sinon = require('sinon');
+
+describe('Error Handler', () => {
+  it('should handle HTTP errors', () => {
+    const error = HttpError.NotFound('Foo bar');
+
+    const stubResponse = {
+      json: sinon.spy(() => stubResponse),
+      status: sinon.spy(() => stubResponse),
+    };
+
+    const result = middleware(error, {}, stubResponse, sinon.spy());
+
+    expect(stubResponse.status.calledWith(404)).to.equal(true);
+
+    expect(stubResponse.json.calledWith({ status: 404, description: 'Not Found', message: 'Foo bar' })).to.equal(true);
+    expect(result).to.equal(stubResponse);
+  });
+
+  it('should handle non HTTP errors', () => {
+    const error = new Error('Some error with sensitive details');
+
+    const stubResponse = {
+      json: sinon.spy(() => stubResponse),
+      status: sinon.spy(() => stubResponse),
+    };
+
+    const result = middleware(error, {}, stubResponse, sinon.spy());
+
+    expect(stubResponse.status.calledWith(500)).to.equal(true);
+
+    expect(stubResponse.json.calledWith({ status: 500, description: 'Internal Server Error', message: 'Sorry, something went wrong' })).to.equal(true);
+    expect(result).to.equal(stubResponse);
+  });
+});
